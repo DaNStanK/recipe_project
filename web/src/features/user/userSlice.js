0d4919dd9efe684345339713e774c8414ada22bb
DaNStanK <daniel.stankovski@gmail.com>
2023-01-05 18:04:27 +0100

    Login create async thunk and builder added

--- web/src/features/user/userSlice.js
+++ web/src/features/user/userSlice.js
@@ -1,34 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { loginUser } from "../../fetch/fetchUsers";
 
 const storageUser = JSON.parse(localStorage.getItem('user'));
 
-export const loginUser = createAsyncThunk('users/loginUsers', async (data) => {
+export const fetchLoginUser = createAsyncThunk('users/fetchLoginUser', async (arg, thunkAPI) => {
    try {
-      let out = await fetch(
-         '/api/v1/auth/login',
-         {
-            method: 'post',
-            body: JSON.stringify(data),
-            headers: {
-               'Content-Type': 'application/json'
-            }
-         }
-      );
-      // check if the fetch was successful   
-      if (!out.ok) {
-         return new Error(out.statusText);
-      }
-
-      let res = await out.json();
-      let u = {
-         user: data.email,
-         token: res.token,
-         isLoggedIn: true
-      };
-      localStorage.setItem('user', JSON.stringify(u));
-      return u;
+      const response = await loginUser(arg);
+      return response;
    } catch (err) {
-      return console.log(err.message);
+      console.log(err);
+      return thunkAPI.rejectWithValue(err);
    }
 });
 
@@ -46,39 +27,26 @@ export const userSlice = createSlice({
    name: 'user',
    initialState,
    reducers: {
-      setLogin: (state, action) => {
-         return {
-            ...state,
-            user: action.payload
-         };
-      },
       setLogout: (state) => {
          return {
             ...state,
             user: null,
             token: null,
             isLoggedIn: null
-         }
+         };
       }
    },
    extraReducers: (builder) => {
       builder
-         .addCase(loginUser.pending, (state) => {
-            state.status = 'loading';
-         })
-         .addCase(loginUser.fulfilled, (state, action) => {
+         .addCase(fetchLoginUser.fulfilled, (state, action) => {
             state.status = 'succeeded';
-            state.user = action.payload;
-         })
-         .addCase(loginUser.rejected, (state, action) => {
-            state.status = 'failed';
-            state.error = action.error.message;
-         })
+            state.user.push(action.payload);
+         });
    }
 });
 
 export const getUser = (state) => state.user;
 
-export const { setLogin, setLogout } = userSlice.actions;
+export const { setLogout } = userSlice.actions;
 
 export default userSlice.reducer;
