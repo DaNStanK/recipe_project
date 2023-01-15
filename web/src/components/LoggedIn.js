8e4713413b343d3d50d20065acedc52408ac0ce3
DaNStanK <daniel.stankovski@gmail.com>
2023-01-15 17:46:00 +0100

    LogOut redux reducer added instead of handleClick

--- web/src/components/LoggedIn.js
+++ web/src/components/LoggedIn.js
@@ -1,15 +1,10 @@
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
 import { useDispatch } from "react-redux";
-import { setLogout } from "../features/user/userSlice";
+import { logoutUser } from "../features/user/userSlice";
 
 export const LoggedIn = () => {
   const dispatch = useDispatch();
-
-  const handleClick = () => {
-    localStorage.clear();
-    dispatch(setLogout);
-  };
 
   return (
     <div className="navbar__logged-users">
@@ -17,7 +12,7 @@ export const LoggedIn = () => {
       <div className='navbar-types__login-circles'></div>
       <Link className="login-links two" to='/users/edit'>MY PROFILE</ Link>
       <div className='navbar-types__login-circles'></div>
-      <Link className="login-links three" to='/users' onClick={handleClick}>LOG OUT</Link>
+      <Link className="login-links three" to='/users' onClick={() => dispatch(logoutUser())}>LOG OUT</Link>
     </div>
   );
 };
