1e909233bf0246f28be837bf87deafb106c9a6c9
DaNStanK <daniel.stankovski@gmail.com>
2023-01-16 19:04:42 +0100

    Added likes onClick update function

--- web/src/features/recipes/RecipesBody.js
+++ web/src/features/recipes/RecipesBody.js
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../user/userSlice";
+import { fetchUpdateRecipe } from "./recipesSlice";
 
 import arrowIcon from "../../icons/icon_arrows_white.svg";
 import plateIcon from "../../icons/icon_plate.svg";
@@ -8,10 +11,20 @@ import clockIcon from "../../icons/icon_time.svg";
 import { ViewRecipe } from "./ViewRecipe";
 
 export const RecipesBody = ({ recipe }) => {
+   const dispatch = useDispatch();
+   const { user } = useSelector(getUser);
    const [view, setView] = useState(false);
 
+   const handleClick = useCallback((e) => {
+      e.preventDefault();
+      dispatch(fetchUpdateRecipe({
+         data: { ...recipe, likes: recipe.likes + 1 },
+         recipeID: recipe._id
+      }));
+   }, [dispatch, recipe]);
+
    return (
-      <div className="recipe">
+      <article className="recipe">
          <img src={require(`../../uploads/${recipe.image_url}`)} alt="recipe pic" className="recipe__image" />
          <div className="recipe_descriptionBox">
             <span>{recipe.title}</span>
@@ -26,7 +39,16 @@ export const RecipesBody = ({ recipe }) => {
                   <h3>{recipe.number_persons} persons</h3>
                </div>
                <div className="recipe__container">
-                  <img src={starIcon} className="star-icon" alt="star pic" />
+                  {!user?.isLoggedIn &&
+                     <img src={starIcon} className="star-icon" alt="star pic" />
+                  }
+                  {user?.isLoggedIn &&
+                     <img
+                        src={starIcon}
+                        className="star-icon" alt="star pic"
+                        onClick={handleClick}
+                     />
+                  }
                   <h3>{recipe.likes}</h3>
                </div>
             </div>
@@ -42,6 +64,6 @@ export const RecipesBody = ({ recipe }) => {
             <div className="modal">
                <ViewRecipe recipe={recipe} setView={setView} />
             </div>}
-      </div>
+      </article>
    );
 };
