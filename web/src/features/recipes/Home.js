bef41070f7cc3ab5ca990a217b9940150740d3c8
DaNStanK <daniel.stankovski@gmail.com>
2023-01-16 21:52:00 +0100

    Added view by categories, fresh and most popular

--- web/src/features/recipes/Home.js
+++ web/src/features/recipes/Home.js
@@ -6,16 +6,39 @@ import { getRecipes } from "./recipesSlice";
 export const Home = () => {
    const recipes = useSelector(getRecipes);
 
+   const recipesByNewestDate = recipes
+      .map(recipe => recipe = { ...recipe, created_on: Date.parse(recipe.created_on) })
+      .sort((a, b) => b.created_on - a.created_on)
+      .splice(0, 3);
+
+   const recipesByMostLikes = recipes.map(recipe => recipe).sort((a, b) => b.likes - a.likes);
+
    return (
       <div className="container">
          <div className="container__tittleBox">
-            <h2 className="container__title">Home</h2>
+            <h2 className="container__title">Fresh & new</h2>
             <div className="container__afterTitle"></div>
          </div>
 
-         {recipes &&
+         {recipesByNewestDate &&
             <div className="container__recipes">
-               {recipes.map(recipe => (
+               {recipesByNewestDate.map(recipe => (
+                  <RecipesBody
+                     key={recipe._id}
+                     recipe={recipe}
+                  />
+               ))}
+            </div>
+         }
+
+         <div className="container__tittleBox">
+            <h2 className="container__title">Most Popular Recipes</h2>
+            <div className="container__afterTitle"></div>
+         </div>
+
+         {recipesByMostLikes &&
+            <div className="container__recipes">
+               {recipesByMostLikes.map(recipe => (
                   <RecipesBody
                      key={recipe._id}
                      recipe={recipe}
