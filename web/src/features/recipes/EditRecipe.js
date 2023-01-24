d58f0a35f51a67a1b7dcff8d83db05cf4e0017d1
DaNStanK <daniel.stankovski@gmail.com>
2023-01-24 18:17:58 +0100

    Getting recipe from recipeSlice instead with fetch

--- web/src/features/recipes/EditRecipe.js
+++ web/src/features/recipes/EditRecipe.js
@@ -1,35 +1,17 @@
 import "./CreateRecipes.css";
 import { useParams, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 import { RecipeEditForm } from "./RecipeEditForm";
 import { useSelector } from "react-redux";
-import { getRecipe } from "../../fetch/fetchRecipes";
-import { getUser } from "../user/userSlice";
 import backIcon from "../../icons/icon_back_white.svg";
-// import { getRecipes } from "./recipesSlice";
+import { getRecipes } from "./recipesSlice";
 
 export const EditRecipe = () => {
 
-   // const recipes = useSelector(getRecipes);
-   const { user } = useSelector(getUser);
    const { recipeID } = useParams();
-   const [recipe, setRecipe] = useState('');
 
-   // on reload of the page there are no recipes?? have to check even so i have dispatch(fetchRecipes()) added on index.js load
-
-   // useEffect(() => {
-   //    const newRecipe = recipes
-   //       .filter(recipe => recipe._id === recipeID)
-   //       .reduce((acc, curr) => curr, {});
-   //    setRecipe(prevState => prevState = newRecipe);
-   // }, [recipeID, recipes]);
-
-   useEffect(() => {
-      (async () => {
-         let newRecipe = await getRecipe(recipeID, user?.token);
-         setRecipe(prevState => prevState = newRecipe);
-      })();
-   }, [recipeID, user?.token]);
+   const recipe = useSelector(getRecipes)
+      .filter(recipe => recipe._id === recipeID)
+      .reduce((acc, curr) => curr, {});
 
    return (
       <div className="container">
