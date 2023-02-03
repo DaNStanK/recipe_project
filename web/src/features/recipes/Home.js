9e22e788563685646efcbd83f7b596a6d98bbfaf
DaNStanK <daniel.stankovski@gmail.com>
2023-02-03 17:09:58 +0100

    Adjust CSS styles around home component

--- web/src/features/recipes/Home.js
+++ web/src/features/recipes/Home.js
@@ -15,38 +15,40 @@ export const Home = () => {
       .sort((a, b) => b.likes - a.likes);
 
    return (
-      <div className="container">
-         <div className="container__tittleBox">
-            <h2 className="container__title">Fresh & new</h2>
-            <div className="container__afterTitle"></div>
-         </div>
-
-         {recipesByNewestDate &&
-            <div className="container__recipes">
-               {recipesByNewestDate.map(recipe => (
-                  <RecipesBody
-                     key={recipe._id}
-                     recipe={recipe}
-                  />
-               ))}
+      <div className="home">
+         <div className="container">
+            <div className="container__tittleBox">
+               <h2 className="container__title">Fresh & new</h2>
+               <div className="container__afterTitle"></div>
             </div>
-         }
 
-         <div className="container__tittleBox">
-            <h2 className="container__title">Most Popular Recipes</h2>
-            <div className="container__afterTitle"></div>
-         </div>
+            {recipesByNewestDate &&
+               <div className="container__recipes">
+                  {recipesByNewestDate.map(recipe => (
+                     <RecipesBody
+                        key={recipe._id}
+                        recipe={recipe}
+                     />
+                  ))}
+               </div>
+            }
 
-         {recipesByMostLikes &&
-            <div className="container__recipes">
-               {recipesByMostLikes.map(recipe => (
-                  <RecipesBody
-                     key={recipe._id}
-                     recipe={recipe}
-                  />
-               ))}
+            <div className="container__tittleBox">
+               <h2 className="container__title">Most Popular Recipes</h2>
+               <div className="container__afterTitle"></div>
             </div>
-         }
+
+            {recipesByMostLikes &&
+               <div className="container__recipes">
+                  {recipesByMostLikes.map(recipe => (
+                     <RecipesBody
+                        key={recipe._id}
+                        recipe={recipe}
+                     />
+                  ))}
+               </div>
+            }
+         </div>
       </div>
    );
 };
