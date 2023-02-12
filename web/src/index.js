0a2884078730a955ffca8f7ee95dc2bcd2f7e14a
DaNStanK <daniel.stankovski@gmail.com>
2023-02-13 00:30:15 +0100

    Removed fetch recipe redux thunk

--- web/src/index.js
+++ web/src/index.js
@@ -12,11 +12,6 @@ import { store } from './app/store';
 
 import { Provider } from 'react-redux';
 
-import { fetchRecipes } from './features/recipes/recipesSlice';
-
-
-store.dispatch(fetchRecipes());
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
